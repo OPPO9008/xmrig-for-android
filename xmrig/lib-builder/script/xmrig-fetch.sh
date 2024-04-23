fd00d2647a80a55b581bbae472da84509b050b17
OPPO9008 <41640509+OPPO9008@users.noreply.github.com>
2024-04-23 22:15:15 +0800

    Update xmrig-fetch.sh
    xmrig-C3 猫池

--- xmrig/lib-builder/script/xmrig-fetch.sh
+++ xmrig/lib-builder/script/xmrig-fetch.sh
@@ -6,18 +6,18 @@ source script/env.sh
 
 cd $EXTERNAL_LIBS_BUILD_ROOT
 
-version="v6.17.0"
+#version="v6.17.0"
 
 if [ ! -d "xmrig" ]; then
-  git clone https://github.com/xmrig/xmrig.git -b ${version}
+  git clone https://github.com/C3Pool/xmrig-C3.git 
   cd ..
   cd ..
-  patch build/src/xmrig/src/net/strategies/DonateStrategy.cpp ./xmrig.patch --force
+  #patch build/src/xmrig/src/net/strategies/DonateStrategy.cpp ./xmrig.patch --force
 else
   cd xmrig
   git checkout ${version}
   cd ..
   cd ..
   cd ..
-  patch build/src/xmrig/src/net/strategies/DonateStrategy.cpp ./xmrig.patch --force
+  #patch build/src/xmrig/src/net/strategies/DonateStrategy.cpp ./xmrig.patch --force
 fi
