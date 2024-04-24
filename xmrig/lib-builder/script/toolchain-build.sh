7c28ba6896930eac2223eb1660dc6dfe206bcec6
OPPO9008 <41640509+OPPO9008@users.noreply.github.com>
2024-04-25 01:20:54 +0800

    Update toolchain-build.sh

--- xmrig/lib-builder/script/toolchain-build.sh
+++ xmrig/lib-builder/script/toolchain-build.sh
@@ -13,7 +13,6 @@ archs=(arm arm64 x86 x86_64)
 for arch in ${archs[@]}; do
     if [ ! -d "$NDK_TOOL_DIR/$arch" ]; then
         echo "installing $ANDROID_NDK_ROOT $arch $args"
-        pip3 install distutils
         make_standalone_toolchain.py $args --arch $arch --install-dir $NDK_TOOL_DIR/$arch
         sed -i.orig "s|using ::fgetpos;|//using ::fgetpos;|" $NDK_TOOL_DIR/$arch/include/c++/4.9.x/cstdio
         sed -i.orig "s|using ::fsetpos;|//using ::fsetpos;|" $NDK_TOOL_DIR/$arch/include/c++/4.9.x/cstdio
